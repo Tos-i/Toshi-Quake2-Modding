76181cd912d531fa58bc2ad6638c87b3244066e2
toshi <toshiryes21@gmail.com>
2026-04-12 21:52:46 -0400

    cluster grenade launcher

--- game/p_weapon.c
+++ game/p_weapon.c
@@ -725,7 +725,17 @@ void weapon_grenadelauncher_fire (edict_t *ent)
 	VectorScale (forward, -2, ent->client->kick_origin);
 	ent->client->kick_angles[0] = -1;
 
-	fire_grenade (ent, start, forward, damage, 600, 2.5, radius);
+		fire_grenade (ent, start, forward, damage, 600, 2.5, radius);
+	
+		start[0] += right[0] * 100;
+		start[1] += right[1] * 10;
+		start[2] += right[2] * 10;
+		fire_grenade(ent, start, forward, damage, 600, 2.5, radius);
+
+		start[0] -= right[0] * 200;
+		start[1] -= right[1] * 20;
+		start[2] -= right[2] * 20;
+		fire_grenade(ent, start, forward, damage, 600, 2.5, radius);
 
 	gi.WriteByte (svc_muzzleflash);
 	gi.WriteShort (ent-g_edicts);
